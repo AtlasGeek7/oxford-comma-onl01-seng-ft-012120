929fc49ed9a078f8a003cd6fdc5be398c742f044
Khalid T <khalid.tissire@gmail.com>
2020-01-26 02:07:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  len = array.length
+  if len === 2
+    
+    array[-1].insert(0, "and ")
+  elsif 2 < len
+    return "#{array[0]} and #{array[1]}"
+  end
+  return array.join(", ")    
 end
